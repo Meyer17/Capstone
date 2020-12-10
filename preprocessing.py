5b83660715ea534a27d405c83f6d03b464e215af
Benny-Benigno <benignovc@berkeley.edu>
2020-12-10 14:33:12 -0800

    Refactored Preprocessing Module

--- preprocessing.py
+++ preprocessing.py
@@ -8,143 +8,25 @@ from dataclasses import dataclass
 
 @dataclass
 class Frame:
-    """ Frame Data Class represents a frame 
+    """ Raw digital audio frame with subsidiary metadata
 
         Attributes
         ----------
         data : np.array
             the segment of samples from the parent wav file
         silent : bool
-            a flag used to indicate if this frame is silent
-        framelen : int
-            the number of samples that is held in this frame
+            a flag used to indicate if a frame instance is silent
         track_index : int
             the global time index of this frame with respect to the audio track
             it derives from
+        smpl_rate : int
+            the sample rate of the digital audio frame in units of Hz
     """
-    data: np.ndarray = np.array([])
+    data: np.ndarray = np.zeros(1)
     silent: bool = False
     track_index: int = 0
+    smpl_rate : int = 0
 
-
-class AudioPreprocessor:
-    """ AudioPreprocessor class generates a set of audio frames from wav file 
-        so that they could be offered to the next stage of the polyphonic
-        music transcription pipeline.
-
-        Attributes
-        ----------
-        framelen : int
-                number of data points in one frame
-        frame_interval : int
-                distance between frames on the time domain
-        silence_thrs : float
-                threshold on which silence is defined as being lower than
-
-        Methods
-        -------
-        process(wav_file)
-            Reads data from a wav file given its name and returns a set of
-            frames of equal length. 
-
-    """
-
-    def __init__(self, framelen, frame_interval, silence_thrs):
-        # the frame interval does not have to equal the frame length
-        # since some frames may overlap eachother
-        self.framelen = framelen
-        self.frame_interval = frame_interval
-        self.silence_thrs = silence_thrs
-
-    def __is_silent(self, data):
-        """ Detects whether a segment of audio is silent 
-            
-            Parameters
-            ----------
-            data : np.array
-                audio sample data
-        """
-        return np.sqrt(np.mean(data**2)) < self.silence_thrs
-
-    def process(self, wav_file):
-        """ Reads in data from a wav file, zero means and rms normalizes it,
-            and returns a set of discrete data frames
-
-            Parameters
-            ----------
-            wav_file : str
-                the name of the wav file to read from 
-        """
-
-        # read wav file data
-        try:
-            smpl_rate, data = wavfile.read(wav_file)
-            print("Sample Rate: {}".format(smpl_rate))
-            print("Number of Samples: {}".format(len(data)))
-
-            data = np.mean(data, axis=1)
-            freqs, times, Sx = spectrogram(data, fs=smpl_rate, window='hanning',
-                                      nperseg=2048, noverlap=2048 - 441,
-                                      detrend=False, scaling='spectrum')
-            print(Sx.shape)
-            Sx = np.sqrt(Sx)
-            print(Sx)
-
-
-        except FileNotFoundError:
-            print("Error: File Not Found")
-        else:
-            f, ax = plt.subplots(figsize=(7, 4))
-            #ax.pcolormesh(times, freqs/1000, 10 * np.log10(Sx), cmap='viridis')
-            #ax.pcolormesh(Sx)
-
-            t = np.arange(Sx.shape[1])
-            t = t/smpl_rate * 1000 # milliseconds
-
-            for i in range(Sx.shape[0]):
-                plt.plot(t, Sx[i])
-                plt.show()
-            
-
-            ax.set_ylabel('Frequency [kHz]')
-            ax.set_xlabel('Time [ms]')
-            plt.show()
-
-            # take the average if the input is stereo channel
-            if data.shape[1] == 2:
-                data = (data[:, 0] + data[:, 1])/2.0
-
-            # zero mean
-            data -= np.mean(data)
-
-            # rms normalization
-            data /= np.sqrt(np.mean(data**2))
-
-            # frame creation
-            L = len(data)
-            self.num_frames = int(np.floor(L/self.frame_interval) + 1)
-            frames = []
-
-            # window function
-            win = hamming(self.framelen)
-
-            for i in range(self.num_frames):
-                findex = i*self.frame_interval
-                frame_data = data[findex : findex + self.framelen]
-                frame_data *= win[0 : len(frame_data)]
-                is_silent = self.__is_silent(frame_data)
-                frames.append(Frame(frame_data, is_silent, findex))
-            
-            print("Number of Frames: {}".format(len(frames)))
-
-            # plot data
-            if __debug__:
-                plt.plot(data)
-                plt.show()
-            
-            return frames
-
-# returns the midi number for a pitch in htz
 def midi(pitch):
     """
         Returns the midi number representation for a pitch
@@ -160,6 +42,88 @@ def midi(pitch):
         midi : int
             The nearest midi number corresponding to the frequency.
     """
-    if pitch <= 0:
-        return 0
-    return int(12 * np.log2(pitch/440) + 69)
+    p = np.around(12 * np.log2(pitch/440) + 69)
+    return np.int32(p)
+
+def dB(amplitude):
+    """
+        Returns the log-scale amplitude for a linear amplitude value.
+
+        Parameters
+        ----------
+        amplitude : float
+            linear amplitude value
+        
+        Returns
+        -------
+        dB : float
+            The log-scale measure(decibel) of the linear amplitude value. 
+    """
+    if len(amplitude) == 0:
+        return amplitude
+    if amplitude.all() <= 0:
+        return np.zeros(len(amplitude))
+    return 20 * np.log10(amplitude)
+
+def process(wav_file, framelen, frame_interval, silence_thrs=.075):
+    """ Reads in data from a wav file, zero means and rms normalizes it,
+        and returns a set of discrete data frames with audio in units of dB
+
+        Parameters
+        ----------
+        wav_file : str
+            the name of the wav file to read from
+    """
+
+    # read wav file data
+    try:
+        smpl_rate, data = wavfile.read(wav_file)
+        print("Sample Rate: {}".format(smpl_rate))
+        print("Number of Samples: {}".format(len(data)))
+
+    except FileNotFoundError:
+        print("Error: File Not Found")
+    else:
+        # take the average if the input is stereo channel
+        if data.shape[1] == 2:
+            data = (data[:, 0] + data[:, 1])/2.0
+
+        # zero mean
+        data -= np.mean(data)
+
+        # rms normalization
+        data /= np.sqrt(np.mean(data**2))
+
+        # frame creation
+        L = len(data)
+        num_frames = int(np.floor(L/frame_interval) + 1)
+        frames = []
+
+        # window function
+        win = hamming(framelen)
+
+        for i in range(num_frames):
+            findex = i*frame_interval
+            frame_data = data[findex : findex + framelen]
+            frame_data *= win[0 : len(frame_data)]
+            is_silent = __is_silent(frame_data, silence_thrs)
+            frames.append(Frame(frame_data, is_silent, findex))
+        
+        print("Number of Frames: {}".format(len(frames)))
+
+        # plot data
+        if __debug__:
+            plt.plot(data)
+            plt.show()
+        
+        return frames
+
+def __is_silent(data, silence_thrs):
+    """ Detects whether a segment of audio is silent 
+        
+        Parameters
+        ----------
+        data : np.array
+            audio sample data
+    """
+    return np.sqrt(np.mean(data**2)) < silence_thrs
