b60487716c801d23d7c35be5a7b986b8458556d3
Benny-Benigno <benignovc@berkeley.edu>
2020-12-10 14:25:19 -0800

    Corrected Peak Detection Algorithm

--- peak_detection.py
+++ peak_detection.py
@@ -1,70 +1,51 @@
+from preprocessing import midi, dB
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import argrelextrema
 from scipy.ndimage import gaussian_filter1d
 
-
-class PeakDetector:
-    """ PeakDetector class finds the local maximum of the spectrogram frame by
-        first convolving it with a gaussian function and then filtering it
-        through selected thresholds. There are three parameters in total for
-        this method of peak detection; them being the gaussian, the base line
-        noise threshold, and the selection threshold.
-
-        Attributes
-        ----------
-        peak_thrs : float
-            threshold on which peaks must be greater than in value
-        noise_thrs : float
-            threshold on which data less than should be ignored
-        gauss_sigma : float
-            the standard deviation of the gaussian function that will
-            be convolved with the spectrogram of the audio frame. 
-            (default is 1.0)
+def get_peaks(frame, peak_thrs=8, noise_thrs=50, gauss_sigma=8.0, padding_fctr=4):
+    """ Returns a set of peaks with their frequency and amplitude values
+        from the smoothed gaussian convolved spectrogram frame.
         
-        Methods
-        -------
-        get_peaks(frame)
-            Returns a set of peak frequency and amplitude points of the
-            spectrogram of the audio frame.
+        Parameters
+        ----------
+        frame : Frame
+            Raw audio frame for analysis
     """
+    # get frequency frame and convolve with gaussian
+    freq_frame = dB(np.absolute(__sfft(frame.data, padding_fctr)))
+    freq_frame = freq_frame[:int(len(freq_frame)/2)]
+    smooth_freq_frame = __convolve_freq_frame(freq_frame, gauss_sigma)
 
-    def __init__(self, max_thrs, noise_thrs, gauss_sigma=1.0, padding_fctr=4):
-        """ 
-            Parameters
-            ----------
-            max_thrs : float
-                threshold on which peaks must be greater than in value
-            noise_thrs : float
-                threshold on which data less than should be ignored
-            gauss_sigma : float, optional
-                the standard deviation of the gaussian function that will
-                be convolved with the spectrogram of the audio frame.
-                (default is 1.0)
-            padding_fctr : float, optional
-                The padding factor is a factor applied to the length of
-                an audio frame to determine the zero padded length of the
-                spectrogram of that audio frame. (default is 4)
-        """
-        self.peak_thrs = max_thrs
-        self.noise_thrs = noise_thrs
-        self.gauss_sigma = gauss_sigma
-        self.padding_fctr = padding_fctr
+    # get extrema
+    peak_freqs = argrelextrema(smooth_freq_frame, np.greater)[0]
 
-    def get_peaks(self, frame):
-        """ Returns a set of peaks with their frequency and amplitude values
-            from the smoothed gaussian convolved spectrogram frame.            
-        """
-        padded_spectrogram = self.__convolve_spectrogram(np.absolute(self.__sfft(frame.data)))
-        spectrogram = padded_spectrogram[:int(len(padded_spectrogram)/2)]
-        peaks = argrelextrema(spectrogram, np.greater)
+    # filter peaks through thresholds
+    floor = np.amax(freq_frame[peak_freqs]) - noise_thrs
+    peak_freqs = peak_freqs[freq_frame[peak_freqs] > floor]
+    local_diff = freq_frame[peak_freqs] - smooth_freq_frame[peak_freqs]
+    peak_freqs = peak_freqs[local_diff > peak_thrs]
+    peak_ampls = freq_frame[peak_freqs]
+    
+    # convert frequency peaks to units of midi and return in numpy array
+    peak_midi = midi(peak_freqs)
+    peak_points = np.column_stack((peak_midi, peak_ampls))
 
-        #TODO: filter peaks through thresholds
 
-    def __sfft(self, audio_data):
-        return np.fft.fft(audio_data, len(audio_data)*self.padding_fctr)
+    if __debug__:
+        print(peak_points)
+        plt.plot(freq_frame)
+        plt.plot(smooth_freq_frame)
+        plt.scatter(peak_freqs, peak_ampls)
+        plt.show()
 
-    # smooths spectrogram
-    def __convolve_spectrogram(self, power_frame):
-        return gaussian_filter1d(power_frame, self.gauss_sigma)
+    return peak_points
 
+def __sfft(audio_data, padding_fctr):
+    return np.fft.fft(audio_data, len(audio_data)*padding_fctr)
+
+# smooths spectrogram with a moving average filter
+def __convolve_freq_frame(freq_frame, gauss_sigma):
+    return gaussian_filter1d(freq_frame, sigma=gauss_sigma)
